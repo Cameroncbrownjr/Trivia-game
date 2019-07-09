c512ea80eb87cc99f697c53a20aeec8f113db294
Cameroncbrownjr <cameroncbrownjr@gmail.com>
2019-07-08 22:39:01 -0400

    game was working but now isnt? also need to add timers

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Trivia-game
 10 questions
 9 normal ones
-8 times more fun than the SAT's
+8 hours of my life went into this
 7 words in this sentance on this line
-6 
+6 times more fun than the SAT's
 5 minutes until you better be done
 4 options choose wisely
 3 wishes granted by a genie for getting every question correct
-2
+2 will be spent on attempts for more wishes
 1 definitive anwser of how smart you are (the test tells all dont argue with science)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,125 +1,124 @@
-//trivia game with diffrent states
-var mainMenudiv = $("#mainMenu")
-var playBtn = $("#play")
-var gameDiv = $("#game")
-var anwserH4 = $("#anwser")
-var questionH3 = $("#question")
-var anwserOptionsDiv = $("#anwserOptions")
-var currentQuestionIndex
-var numCorrect
+//trivia game with different states
+var mainMenudiv = $("#mainMenu");
+var playBtn = $("#play");
+var gameDiv = $("#game");
+var answerH4 = $("#answer");
+var questionH3 = $("#question");
+var answerOptionsDiv = $("#answerOptions");
+var currentQuestionIndex;
+var numCorrect;
 var qList = [
     {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
+        question: "What's the capital of the United States?",
+        answer: "Washington, DC",
+        options: ["Washington, DC", "Philadelphia", "Washington State", "Orange monkey island"]
     }
     {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
+        question: "Are ponies just small horses?",
+        answer: "Definitely not",
+        options: ["For sure", "Definitely not", "small evil horses", "Unsure"]
     }
     {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
+        question: "If given the chance, will a cat sit in a box?",
+        answer: "fits=sits",
+        options: ["no", "what is cat?", "fits=sits", "How big/important is the box?"]
     }
     {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
+        question: "What is the closest planet to the sun?",
+        answer: "Mercury",
+        options: ["Venus", "Pluto", "Uranus", "Mercury"]
     }
     {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
+        question: "What is the best hockey team?",
+        answer: "Flyers",
+        options: ["Devils", "Flyers", "Penguins", "Buffalo Bills"]
     }
     {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
+        question: "What does 2+2 equal?",
+        answer: "4",
+        options: ["2.5", "Fish", "4", "I didn't pass kindergarten"]
+    
+        question: "The earth is _____?",
+        answer: "round, dumbass",
+        options: ["flat", "on fiyaaah", "square", "round, dumbass"]
     }
     {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
+        question: "What color is the sky?",
+        answer: "blue",
+        options: ["blue", "red", "green", "purple"]
     }
     {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
+        question: "How many fingers should 3 people have all together?",
+        answer: "30",
+        options: ["35", "40", "30", "29"]
     }
     {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
+        question: "If given the chance to rate this quiz's quality, you'd say it's a ____? ",
+        answer: "10/10s only",
+        options: ["0/10", "0.5/10", "5/10", "10/10s only"]
     }
-    {
-        question: "",
-        anwser: "",
-        options: ["", "", "", ""]
-    }
-]
+];
 
 playBtn.on("click", function (e) {
-    mainMenudiv.hide()
-    gameDiv.show()
-    startGame()
+    mainMenudiv.hide();
+    gameDiv.show();
+    startGame();
 })
 function startGame() {
-    questionH3.text("")
-    anwserH4.text("")
-    anwserOptionsDiv.empty()
-    currentQuestionIndex = 0
-    numCorrect = 0
-    qList.sort(() => 0.5 - Math.random())
+    questionH3.text("");
+    answerH4.text("");
+    answerOptionsDiv.empty();
+    currentQuestionIndex = 0;
+    numCorrect = 0;
+    qList.sort(() => 0.5 - Math.random());
     showQuestion();
 }
 
 function showQuestion() {
-    anwserOptionsDiv.empty()
-    anwserH4.text("")
+    answerOptionsDiv.empty();
+    answerH4.text("");
     var currentQuestion = qList[currentQuestionIndex]
-    questionH3.text(currentQuestion.question)
-    // loop anwser throuh options to make buttons
+    questionH3.text(currentQuestion.question);
+    // loop anwser through options to make buttons
     for (var i = 0; i < currentQuestion.options.length; i++) {
-        var newBtn = $("button")
-        newBtn.text(currentQuestion.options[i])
+        var newBtn = $("button");
+        newBtn.text(currentQuestion.options[i]);
         //attach click listner to button
         newBtn.on("click", function (e) {
             console.log(e.target.textContent)
             //handle correct/inc guess
-            if (e.target.textContent === currentQuestion.anwser) {
-                numCorrect++
-                anwserH4.text("You are so " + currentQuestion.anwserOptionsDiv)
+            if (e.target.textContent === currentQuestion.answer) {
+                numCorrect++;
+                answerH4.text("You are so " + currentQuestion.answerOptionsDiv);
             }
             else {
-                anwserH4.text("wrong")
+                answerH4.text("wrong");
             }
             //create next question button
-            anwserOptionsDiv.empty()
-            var nextBtn = $(#button)
-            nextBtn.text("next Queston")
-            nextBtn.on("click", showNextQuestion)
-            anwserOptionsDiv.append(nextBtn)
+            answerOptionsDiv.empty()
+            var nextBtn = $(#button);
+            nextBtn.text("next Queston");
+            nextBtn.on("click", showNextQuestion);
+            answerOptionsDiv.append(nextBtn);
         })
-        anwserOptionsDiv.append(newBtn)
+        answerOptionsDiv.append(newBtn)
     }
 }
 //if next question is outside of length then end game
 function showNextQuestion() {
     if (currentQuestionIndex + 1 !== qList.length) {
-        currentQuestionIndex++
-        showQuestion()
+        currentQuestionIndex++;
+        showQuestion();
     }
     else {
-        gameOver()
+        gameOver();
     }
 }
 
 function gameOver() {
-    gameDiv.hide()
-    $("#mainMenu").text("you Scored " + Math.round(numCorrect / qList.length) + "%")
-    $("#mainMenu").text("")
-    playBtn.text("Play again")
-    mainMenudiv.show()
+    gameDiv.hide();
+    $("#mainMenu").text("you Scored " + Math.round(numCorrect / qList.length) + "%");
+    $("#mainMenu").text("");
+    playBtn.text("Play again");
+    mainMenudiv.show();
 }
